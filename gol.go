0a5f48e271226db5f3a4c83047de6684fda053da
louie <louie@nate.tech>
2019-11-25 16:57:49 +0000

    1b started

--- gol.go
+++ gol.go
@@ -15,6 +15,12 @@ func distributor(p golParams, d distributorChans, alive chan []cell) {
 		world[i] = make([]byte, p.imageWidth) //two of these, one for source, one for target
 	}
 
+		// Create the 2D slice to store the new world.
+		new_world := make([][]byte, p.imageHeight)
+		for i := range world {
+			new_world[i] = make([]byte, p.imageWidth)
+		}
+
 	// Request the io goroutine to read in the image with the given filename. //these lines chnage to output, send to outpuVal
 	d.io.command <- ioInput
 	d.io.filename <- strings.Join([]string{strconv.Itoa(p.imageWidth), strconv.Itoa(p.imageHeight)}, "x")
@@ -22,7 +28,7 @@ func distributor(p golParams, d distributorChans, alive chan []cell) {
 	// The io goroutine sends the requested image byte by byte, in rows.
 	for y := 0; y < p.imageHeight; y++ {
 		for x := 0; x < p.imageWidth; x++ {
-			val := <-d.io.inputVal //mkae this cannel no directionsla anymore
+			val := <-d.io.inputVal 
 			if val != 0 {
 				fmt.Println("Alive cell at", x, y)
 				world[y][x] = val
@@ -36,31 +42,90 @@ func distributor(p golParams, d distributorChans, alive chan []cell) {
 			for x := 0; x < p.imageWidth; x++ {
 				// Placeholder for the actual Game of Life logic: flips alive cells to dead and dead cells to alive.
 				// world[y][x] = world[y][x] ^ 0xFF
-				var sum = 0
+				workerHeight := p.imageHeight / p.threads
+				out := make([]chan [][]uint8, p.threads)
+				for i := range out {
+					out[i] = make(chan [][]uint8) //meemory changes -> needs to chnage
+				}
+				
+				for i := 0; i < p.threads; i++ {
+					start := i*workerHeight
+					slice_world[i] = make([]byte, workerHeight + 2)
+					top := start - 1
+					if start == 0 {
+						top = p.imageHeight - 1
+					}
+					bottom := start + workerHeight
+					if start == p.imageHeight - workerHeight {
+						bottom = 0
+					}
+					
+					slice_world[i] = append(slice_world[i], top)
+					for s:= start; s < bottom; s++ {
+						slice_world[i] = append(slice_world[i], s)
+					}
+					slice_world[i] = append(slice_world[i], bottom)
+					go worker(slice_world[i])
+				}
+			}
+		}
+	}
 
-				if (x == 0) || (y == 0) || (x == p.imageWidth -1) || (y == p.imageHeight -1) {
+	d.io.inputVal <- new_world[y][x]
 
-					var yplus = 0
-					var yminus = 0
-					var xplus = 0
-					var xminus = 0
+
+	// Create an empty slice to store coordinates of cells that are still alive after p.turns are done.
+	var finalAlive []cell
+	// Go through the world and append the cells that are still alive.
+	for y := 0; y < p.imageHeight; y++ {
+		for x := 0; x < p.imageWidth; x++ {
+			if world[y][x] != 0 {
+				finalAlive = append(finalAlive, cell{x: x, y: y})
+			}
+		}
+	}
+
+	// Make sure that the Io has finished any output before exiting.
+	d.io.command <- ioCheckIdle
+	<-d.io.idle
+
+	// Return the coordinates of cells that are still alive.
+	alive <- finalAlive
+}
+
+func worker(world[y][x], d distributorChans){
+	var sum = 0
+				var maxWidth = p.imageWidth - 1
+				var maxHeight = p.imageHeight -1
+				fmt.Println(y,x)
+
+				if (x == 0) || (y == 0) || (x == maxWidth) || (y == maxHeight) {
+
+					var yplus = y + 1
+					var yminus = y - 1
+					var xplus = x + 1
+					var xminus = x - 1
 
 					if (y == 0) {
+						fmt.Println(y, "here 1")
 						yplus = y+1
-						yminus = p.imageHeight -1
+						yminus = maxHeight
 					}
 	
-					if (y == p.imageHeight -1) {
+					if (y == maxHeight) {
+						fmt.Println(y, "here4")
 						yplus = 0
-						yminus = y-1
+						yminus = y-1  
 					}
 	
 					if (x == 0) {
+						fmt.Println(x,"here3")
 						xplus = x+1
-						xminus = p.imageWidth -1
+						xminus = maxWidth
 					}
 	
-					if (x == p.imageWidth -1) {
+					if (x == maxWidth) {
+						fmt.Println(x, "here 2")
 						xplus = 0
 						xminus = x-1
 					}
@@ -90,35 +155,15 @@ func distributor(p golParams, d distributorChans, alive chan []cell) {
 				}
 
 				if sum < 2 && (world[y][x] == 0xFF) {
-					world[y][x] = world[y][x] ^ 0xFF
+					world[y][x] = new_world[y][x] ^ 0xFF
 				} else if sum == 2 || sum == 3 && (world[y][x] == 0xFF) {
 
 				} else if sum == 3 && world[y][x] != 0xFF {
-					world[y][x] = world[y][x] ^ 0xFF
+					world[y][x] = new_world[y][x] ^ 0xFF
 				} else if sum > 3 && (world[y][x] == 0xFF) {
-					world[y][x] = world[y][x] ^ 0xFF
+					world[y][x] = new_world[y][x] ^ 0xFF
 				} else {
 					
 				}
-			}
-		}
-	}
-
-	// Create an empty slice to store coordinates of cells that are still alive after p.turns are done.
-	var finalAlive []cell
-	// Go through the world and append the cells that are still alive.
-	for y := 0; y < p.imageHeight; y++ {
-		for x := 0; x < p.imageWidth; x++ {
-			if world[y][x] != 0 {
-				finalAlive = append(finalAlive, cell{x: x, y: y})
-			}
-		}
-	}
-
-	// Make sure that the Io has finished any output before exiting.
-	d.io.command <- ioCheckIdle
-	<-d.io.idle
-
-	// Return the coordinates of cells that are still alive.
-	alive <- finalAlive
+				d.io.inputVal <- new_world[y][x]
 }
