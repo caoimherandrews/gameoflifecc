f4d7fcf1ac758be1dfea61df517e848fc3abbb0d
louie <louie@nate.tech>
2019-11-26 15:19:14 +0000

    1a - almost there

--- gol.go
+++ gol.go
@@ -40,13 +40,7 @@ func distributor(p golParams, d distributorChans, alive chan []cell) {
 	}
 
 	// Calculate the new state of Game of Life after the given number of turns.
-	for turns := 0; turns < p.turns; turns++ {
-		for y := 0; y < p.imageHeight; y++ {
-			for x := 0; x < p.imageWidth; x++ {
-				golLogic(world, p)
-			}
-		}
-	}
+	 golLogic(world, new_world, p, d)
 
 	// Create an empty slice to store coordinates of cells that are still alive after p.turns are done.
 	var finalAlive []cell
@@ -68,73 +62,79 @@ func distributor(p golParams, d distributorChans, alive chan []cell) {
 	alive <- finalAlive
 }
 
-func golLogic(world, p golParams){
-		var sum = 0
-		var maxWidth = p.imageWidth - 1
-		var maxHeight = p.imageHeight -1
+func golLogic(world [][]byte, new_world [][]byte, p golParams, d distributorChans){
+	for turns := 0; turns < p.turns; turns++ {
+		for y := 0; y < p.imageHeight; y++ {
+			for x := 0; x < p.imageWidth; x++ {
+				var sum = 0
+				var maxWidth = p.imageWidth - 1
+				var maxHeight = p.imageHeight -1
 
-		if (x == 0) || (y == 0) || (x == maxWidth) || (y == maxHeight) {
+				if (x == 0) || (y == 0) || (x == maxWidth) || (y == maxHeight) {
 
-			var yplus = y + 1
-			var yminus = y - 1
-			var xplus = x + 1
-			var xminus = x - 1
+					var yplus = y + 1
+					var yminus = y - 1
+					var xplus = x + 1
+					var xminus = x - 1
 
-			if (y == 0) {
-				yplus = y+1
-				yminus = maxHeight
-			}
+					if (y == 0) {
+						yplus = y+1
+						yminus = maxHeight
+					}
 
-			if (y == maxHeight) {
-				yplus = 0
-				yminus = y-1  
-			}
+					if (y == maxHeight) {
+						yplus = 0
+						yminus = y-1  
+					}
 
-			if (x == 0) {
-				xplus = x+1
-				xminus = maxWidth
-			}
+					if (x == 0) {
+						xplus = x+1
+						xminus = maxWidth
+					}
 
-			if (x == maxWidth) {
-				xplus = 0
-				xminus = x-1
-			}
+					if (x == maxWidth) {
+						xplus = 0
+						xminus = x-1
+					}
 
-			if world[yminus][xminus] == 0xFF {sum = sum + 1}
-			if world[yminus][x] == 0xFF {sum = sum + 1}
-			if world[yminus][xplus] == 0xFF {sum = sum + 1}
+					if world[yminus][xminus] == 0xFF {sum = sum + 1}
+					if world[yminus][x] == 0xFF {sum = sum + 1}
+					if world[yminus][xplus] == 0xFF {sum = sum + 1}
 
-			if world[y][xminus] == 0xFF {sum = sum + 1}
-			if world[y][xplus] == 0xFF {sum = sum + 1}
+					if world[y][xminus] == 0xFF {sum = sum + 1}
+					if world[y][xplus] == 0xFF {sum = sum + 1}
 
-			if world[yplus][xminus] == 0xFF {sum = sum + 1}
-			if world[yplus][x] == 0xFF {sum = sum + 1}
-			if world[yplus][xplus] == 0xFF {sum = sum + 1}
-			
-		} else {
+					if world[yplus][xminus] == 0xFF {sum = sum + 1}
+					if world[yplus][x] == 0xFF {sum = sum + 1}
+					if world[yplus][xplus] == 0xFF {sum = sum + 1}
+					
+				} else {
 
-			for vertical:= -1; vertical < 2; y++ {
-				for horizontal:=-1; horizontal < 2; x++{
-					if world[y+vertical][x+horizontal] == 0XFF {																
-						sum = sum + 1
-					}else {
-						sum = sum
+					for vertical:= -1; vertical < 2; y++ {
+						for horizontal:=-1; horizontal < 2; x++{
+							if world[y+vertical][x+horizontal] == 0XFF {																
+								sum = sum + 1
+							}else {
+								sum = sum
+							}
+						}
 					}
 				}
+
+				if sum < 2 && (world[y][x] == 0xFF) {
+					new_world[y][x] = world[y][x] ^ 0xFF
+				} else if sum == 2 || sum == 3 && (world[y][x] == 0xFF) {
+
+				} else if sum == 3 && world[y][x] != 0xFF {
+					new_world[y][x] = world[y][x] ^ 0xFF
+				} else if sum > 3 && (world[y][x] == 0xFF) {
+					new_world[y][x] = world[y][x] ^ 0xFF
+				} else {
+					
+				}
+				world[y][x] = new_world[y][x]
+				d.io.outputVal <- world[y][x]
 			}
 		}
-
-		if sum < 2 && (world[y][x] == 0xFF) {
-			new_world[y][x] = world[y][x] ^ 0xFF
-		} else if sum == 2 || sum == 3 && (world[y][x] == 0xFF) {
-
-		} else if sum == 3 && world[y][x] != 0xFF {
-			new_world[y][x] = world[y][x] ^ 0xFF
-		} else if sum > 3 && (world[y][x] == 0xFF) {
-			new_world[y][x] = world[y][x] ^ 0xFF
-		} else {
-			
-		}
-		new_world[y][x] = world[y][x]
-		d.io.inputVal <- world[y][x]
+	}
 }
--- pgm.go
+++ pgm.go
@@ -41,7 +41,7 @@ func writePgmImage(p golParams, i ioChans) {
 	// TODO: write a for-loop to receive the world from the distributor when outputting.
 	for y := 0; y < p.imageHeight; y++ {
 		for x := 0; x < p.imageWidth; x++ {
-			val := <-i.distributor.inputVal
+			val := <- i.distributor.outputVal
 			world[y][x] = val	
 		}
 	}
